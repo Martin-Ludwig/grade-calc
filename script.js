3941175b8531459191dee7527216c122a059c6b1
Martin-Ludwig <martin95ludwig@gmail.com>
2019-05-21 15:00:19 +0200

    stylings, js functions

--- script.js
+++ script.js
@@ -1,10 +1,19 @@
 
 $( document ).ready(function() {
 
-    let module_markup = $('.modules-wrapper').html();
+    let module_markup = $('template.module-template').html();
 
+
+
+    //add button
     $('#add-module-btn').click(function() {
-        $('.module').last().append(module_markup);
+        $('.modules-wrapper').last().append(module_markup);
+    });
+
+    //delete button
+    //on function for dynamic objects
+    $( "body" ).on( "click", ".delete-module-btn", function() {
+        $(this).closest('.module').remove();
     });
 
 });
